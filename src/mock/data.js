75cb5527e8b1af7491231fd74a607a5081742840
clarissajaime <clarissanjaime@gmail.com>
2020-11-17 11:55:09 -0800

    customize template with personal data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,73 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Clarissa Jaime | Front-End Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: "Hi, I'm",
+  name: 'Clarissa Jaime.',
+  subtitle: "I'm a front-end engineer that builds great products.",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'As a former digital marketer and UX engineer, I am passionate about building effective and optimal user experiences.',
+  paragraphTwo:
+    'In my current role at 2U, Inc, I work on the learning management management system and build tools to provide course data to faculty and students.',
+  paragraphThree:
+    "When I'm not working, you'll find me taking art classes, including singing, makeup artistry and generative art.",
+  resume:
+    'https://docs.google.com/document/d/108P-DCqisyr_6sgmpWUYAKvubz1_QFljjxAzDUQ20a4/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'dashboard.jpg',
+    title: 'Dotlearn',
+    info:
+      "Dotlearn is 2U's proprietary learning management system. On the Academic Product team, I worked on the dashboard and activity report pages to display course insights to faculty and students.",
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'atrio-ui.jpg',
+    title: 'Atrio-UI',
+    info:
+      "Atrio UI is 2U's new React design system. I worked on the project ideation and developed components for the project.",
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'whitelabel.jpg',
+    title: 'Whitelabel',
+    info:
+      "Whitelabel is 2U's internal asset management application. I was in charge of project documentation, stakeholder communication, and improving the project infrastructure.",
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'capex.jpg',
+    title: 'Capy',
+    info:
+      'Capy, the CapEx Reporting Tool, is an internal finance application that streamlines the capex reporting process for employees. I was a part of the ideation and cross-team communication efforts.',
+    info2: '',
+    url: '',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +76,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'clarissanjaime@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +84,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/clarissajaime/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/clarissajaime/',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
